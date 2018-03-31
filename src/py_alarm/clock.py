b4aa0148106c84a3cae9a54dfc533b2dbabf50fa
Olly Shaw <ollyjshaw@gmail.com>
2018-03-31 23:21:27 +0100

    type hints

--- src/py_alarm/clock.py
+++ src/py_alarm/clock.py
@@ -1,14 +1,18 @@
-class Clock:
-  def sound(self, time_string):
-    time = _Time(time_string)
-    if (time.hours == 7 and time.mins == 0):
-      return "wake up!"
-    elif time.mins == 0:
-      return "beep"
-    else:
-      return "tick"
-
 class _Time:
-  def __init__(self, time):
+  def __init__(self, time:str):
     self.hours = int(time[0:2])
     self.mins = int(time[3:4])
+
+class Clock:
+  def sound(self, time_string:str) -> str:
+    time = _Time(time_string)
+    return self.getString(time)
+
+  #type hints need the type defined further up
+  def getString(self, time:_Time):
+      if time.hours == 7 and time.mins == 0:
+        return "wake up!"
+      elif time.mins == 0:
+        return "beep"
+      else:
+        return "tick"
